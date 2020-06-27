f4c6a5b8efa4b7f07fff17b56750d49a629f9bbf
Elton Hsu <elton@clicktool.com>
2020-06-27 14:04:20 -0700

    Updates

---
+++ precache-manifest.38696cf9802a215de6b22018c3501ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be7442a6fe4826d10b80515f0753b1a",
+    "url": "/kittbio/index.html"
+  },
+  {
+    "revision": "43fcc0397985d2182867",
+    "url": "/kittbio/static/css/main.e6a1f02b.chunk.css"
+  },
+  {
+    "revision": "2e440c472fc921156e0a",
+    "url": "/kittbio/static/js/2.1a5187b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittbio/static/js/2.1a5187b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43fcc0397985d2182867",
+    "url": "/kittbio/static/js/main.3d53928b.chunk.js"
+  },
+  {
+    "revision": "d91c22def33a264e2d37",
+    "url": "/kittbio/static/js/runtime-main.f84cdd3e.js"
+  }
+]);
